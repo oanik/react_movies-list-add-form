b51ee32f5df0e6cd4c03252314f195a9a373d225
Olga Rubalska <olga.anikeieva@gmail.com>
2021-02-09 16:28:07 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2088c32903613710f8f9678a01b40cd4.js"
+  "./precache-manifest.cdcb8023a8a35999e9459fd5f63510e6.js"
 );
 
 self.addEventListener('message', (event) => {
